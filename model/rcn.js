375f453074343e9baee1e51f3fab85c1640258ad
Deepak Jacob <jacob.deepak@yahoo.com>
2016-09-25 23:43:25 +0100

    Rcm model feilds added

--- model/rcn.js
+++ model/rcn.js
@@ -2,7 +2,16 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 const RcnSchema   = new Schema({
-    name: String
+    mark: String,
+    outurn: String,
+    nutCount: String,
+    quantity: String,
+    price: String,
+    port: String,
+    arrivalDate: Date
+    email: String,
+    contact number: String,
+    company: String,
 });
 
 export default mongoose.model('Rcn', RcnSchema);
