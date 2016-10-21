ff267eee7bfd347f6bf2cd867fc8ccc4ade60f93
Deepak Jacob <jacob.deepak@yahoo.com>
2016-10-21 13:01:08 +0100

    render form

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NutsList from './components/NutsList.jsx';
+import NutsAdd from './components/NutsAdd.jsx';
 
 ReactDOM.render(
   <NutsList />,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+  <NutsAdd />,
+  document.getElementById('addn')
 );
