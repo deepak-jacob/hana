56b67906b9ec95592dc2271dbb4842665778c127
Deepak Jacob <jacob.deepak@yahoo.com>
2016-10-22 23:26:42 +0100

    Add nuts submit handler

--- src/components/NutsAdd.jsx
+++ src/components/NutsAdd.jsx
@@ -27,12 +27,32 @@ class NutsAdd extends Component {
     this.setState(change);
   }
 
+  handleSubmit(e) {
+
+    let formData = Object.keys(this.state).map((k) => {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(this.state[k])
+    }).join('&');
+
+    fetch('/rcnuts', {
+      method: 'POST',
+      body: formData,
+      headers: {
+       'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+       'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+       }
+    });
+
+  }
+
   render() {
     return (
       <div>
       { _.map(keysToDisplay, (val, key) => {
-        return <input type="text" />
+        return <input type="text" placeholder={val} style={{display:'block', margin: '5px 0'}}
+          value={this.state[key]}
+          onChange={this.handleChange.bind(this, key)} />
       }) }
+      <button onClick={this.handleSubmit.bind(this)}> Submit</button>
       </div>
     );
   }
