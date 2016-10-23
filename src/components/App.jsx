1be743a5e8249fbcf3a34b0f4dab162e6beb1ade
Deepak Jacob <jacob.deepak@yahoo.com>
2016-10-23 13:11:31 +0100

    creating app navigation

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ul role="nav">
+          <li><Link to="/nutsList">Nuts List</Link></li>
+          <li><Link to="/nutsAdd">Nuts Add</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
+  }
+}
