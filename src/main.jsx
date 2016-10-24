c97030aa1aae1194ddd8867ea258be5878d1fe71
Deepak Jacob <jacob.deepak@yahoo.com>
2016-10-24 13:13:38 +0100

    render app nav

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'
+import App from './components/App.jsx';
 import NutsList from './components/NutsList.jsx';
 import NutsAdd from './components/NutsAdd.jsx';
 
-ReactDOM.render(
-  <NutsList />,
-  document.getElementById('root')
-);
-
-ReactDOM.render(
-  <NutsAdd />,
-  document.getElementById('addn')
-);
+ReactDOM.render((
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <Route path="/nutsList" component={NutsList} />
+        <Route path="/nutsAdd" component={NutsAdd} />
+      </Route>
+    </Router>
+), document.getElementById('root'));
