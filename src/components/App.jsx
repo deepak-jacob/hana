c4e87db235bd54cbec586ce8f753bdaa1ba0965e
Deepak Jacob <jacob.deepak@yahoo.com>
2016-10-25 23:23:11 +0100

    Adding home to links

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ export default class App extends Component {
     return (
       <div>
         <ul role="nav">
+          <li><Link to="/">Home</Link></li>
           <li><Link to="/nutsList">Nuts List</Link></li>
           <li><Link to="/nutsAdd">Nuts Add</Link></li>
         </ul>
