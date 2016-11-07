a375617ed9e18725d1e8acb01b074ceccfb41579
Deepak Jacob <jacob.deepak@yahoo.com>
2016-11-07 13:47:08 +0000

    route config for create path

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 
 import App from './components/App.jsx';
 import NutsList from './components/NutsList.jsx'
+import NutsListContainer from './containers/NutsListContainer.jsx'
 import NutsAdd from './components/NutsAdd.jsx'
 import reducers from './reducers'
 import { getAllNuts } from './actions'
 
+
+console.log(reducers);
+
+const middleware = [ thunk ];
+
 const store = createStore(
   combineReducers({
-    ...reducers,
+    reducers,
     routing: routerReducer
-  })
+  }),
+  applyMiddleware(...middleware)
 );
 
 store.dispatch(getAllNuts())
@@ -26,8 +34,8 @@ ReactDOM.render((
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
-      <Route path="/nutsList" component={NutsList} />
-      //  <Route path="/nutsAdd" component={NutsAdd} />
+      <Route path="/nutsList" component={NutsListContainer} />
+
       </Route>
     </Router>
   </Provider>
