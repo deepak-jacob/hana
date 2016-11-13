b367738cb98b8e48beda125caa3f787dfe71ee73
Deepak Jacob <jacob.deepak@yahoo.com>
2016-11-13 00:47:22 +0000

    redux store changes for nutslist

---
+++ src/reducers/nuts.js
@@ -0,0 +1,15 @@
+import * as types from '../constants/ActionTypes'
+
+const nuts = (state = [], action) => {
+  switch (action.type) {
+    case types.RECEIVE_NUTS:
+      return [...action.nuts]
+    default:
+      return state
+  }
+}
+
+export default nuts
+
+export const getnuts = (state) =>
+  state.reducers
