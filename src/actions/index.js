39862d8aaa04d358ff5dd534f5d1277ca2a4130e
Deepak Jacob <jacob.deepak@yahoo.com>
2016-11-16 14:20:37 +0000

    adding new save action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,3 +12,20 @@ export const getAllNuts = () => dispatch => {
       dispatch(receiveNuts(json))
     });
 }
+
+export const saveNut = () => (dispatch, getState) => {
+  const { form: {NutsAddForm:{values: nutsFormData }} } = getState()
+  const formData = Object.keys(nutsFormData).map((k) => {
+      return encodeURIComponent(k) + '=' + encodeURIComponent(nutsFormData[k])
+  }).join('&');
+
+  fetch('/rcnuts', {
+    method: 'POST',
+    body: formData,
+    headers: {
+     'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+     'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+     }
+  });
+
+}
