b7ab3c6c74cd9b6761761b10a71022b6e9ca58f0
Deepak Jacob <jacob.deepak@yahoo.com>
2016-11-18 14:22:13 +0000

    add container

--- src/containers/NutsAddContainer.jsx
+++ src/containers/NutsAddContainer.jsx
@@ -1,10 +1,9 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
-import NutsList from '../components/NutsAdd.jsx'
+import NutsAdd from '../components/NutsAdd.jsx'
 import { getnuts } from '../reducers/nuts'
+import { saveNut } from '../actions'
 
-const mapStateToProps = store => ({
-  nutsList: getnuts(store)
-})
+const mapStateToProps = store => ({})
 
-export default connect(mapStateToProps)(NutsList)
+export default connect(mapStateToProps, { saveNut })(NutsAdd)
