fa5cc46d82e97484340c02c6f0dd8ddde3d23362
Deepak Jacob <jacob.deepak@yahoo.com>
2017-02-22 17:15:45 +0000

    webpack prod config

---
+++ webpack.prod.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-source-map',
+  entry: [
+    path.join(__dirname, 'src/main.jsx'),
+  ],
+  output: {
+    path: path.join(__dirname),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'commons',
+      filename: 'commons.js',
+      //minChunks: 1,
+    }),
+    new LodashModuleReplacementPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    // new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+  ],
+  module: {
+    loaders: [{
+      loader: 'babel-loader',
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      include: [
+        path.resolve(__dirname, 'src'),
+      ],
+    }, {
+      test: /\.json?$/,
+      loader: 'json-loader',
+    }, {
+      test: /\.css$/,
+      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]',
+    }],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
