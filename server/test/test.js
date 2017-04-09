fb6e159e72e082389e42c8e6550d68e3acb90131
Deepak Jacob <jacob.deepak@yahoo.com>
2017-04-09 04:21:23 +0100

    test for auth and users

--- server/test/test.js
+++ server/test/test.js
@@ -3,20 +3,20 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
-const rcnModel = require('../model/rcn');
+const rcnModel = require('../models/rcn');
 const userModel = require('../models/user');
 
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('rcnModel', () => {
+describe('Rcn', () => {
   beforeEach((done) => { // Before each test we empty the database
     rcnModel.remove({}, () => {
       done();
     });
   });
 
-  describe('/GET rcnuts', () => {
+  describe('/GET list', () => {
     it('it should GET all the rcnuts', (done) => {
       chai.request(server)
           .get('/rcnuts')
@@ -29,7 +29,7 @@ describe('rcnModel', () => {
     });
   });
 
-  describe('/POST rcnuts', () => {
+  describe('/POST create', () => {
     it('it should POST a rcnuts ', (done) => {
       const rcnut = {
         mark: 'test mark',
@@ -56,42 +56,74 @@ describe('rcnModel', () => {
   });
 });
 
-describe('usermodel', () => {
-  describe('/GET users', () => {
-    it('it should GET all the users', (done) => {
+describe('Users', () => {
+  let authToken;
+
+  describe('create', () => {
+    beforeEach((done) => { // Before each test we empty the database
+      userModel.remove({}, () => {
+        done();
+      });
+    });
+
+    describe('/POST create', () => {
+      it('it should create a new user', (done) => {
+        const userObj = {
+          email: 'test@test.com',
+          password: 'testpass',
+          fname: 'firstname',
+          lname: 'secname',
+        };
+        chai.request(server)
+          .post('/auth/signup')
+          .send(userObj)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('success').eql(true);
+            res.body.should.have.property('message').eql('You have successfully signed up! Now you should be able to log in.');
+            done();
+          });
+      });
+    });
+  });
+
+  describe('login', () => {
+    describe('/POST auth login', () => {
+      it('it should login user', (done) => {
+        const userObj = {
+          email: 'test@test.com',
+          password: 'testpass',
+        };
+        chai.request(server)
+          .post('/auth/login')
+          .send(userObj)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('success').eql(true);
+            res.body.should.have.property('token');
+            res.body.should.have.property('message').eql('You have successfully logged in!');
+            authToken = res.body.token;
+            done();
+          });
+      });
+    });
+  });
+
+  describe('/GET list', () => {
+    it('it should list all the users', (done) => {
       chai.request(server)
           .get('/users')
+          .set('authorization', `Bearer ${authToken}`)
           .end((err, res) => {
             res.should.have.status(200);
             res.body.should.be.a('array');
-            res.body.length.should.be.eql(0);
+            res.body.length.should.be.eql(1);
+            res.body[0].email.should.be.eql('test@test.com');
             done();
           });
     });
   });
 });
 
-describe('auth', () => {
-  describe('/POST auth signup', () => {
-    it('it should create a new user', (done) => {
-      userModel.remove({}, () => {
-        done();
-      });
-      const userObj = {
-        email: 'deepak1@deepak.com',
-        password: 'password',
-        fname: 'deepak',
-        lname: 'jacob',
-      };
-      chai.request(server)
-          .post('/auth/signup')
-          .send(userObj)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('success').eql('true');
-            done();
-          });
-    });
-  });
-});
